2e50687784f8b4d9d733223ac04bde85cba3149c
teekize <elvisteeka1@gmail.com>
2018-11-10 08:50:51 +0300

    Basic Skeleton for the web app
    Added code to the file to enable retrieving of data when signing up via google sign-in

--- run.py
+++ run.py
@@ -1,4 +1,22 @@
-from app import app
+from flask import Flask, render_template,request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    #returns the landing page
+  return render_template("mainSite.html")
+
+@app.route('/name/',methods=['POST'])
+def post_me():
+  email = request.form['email']
+  password = request.form['password']
+  #TODO:to check with db if successfull
+  return render_template('two.html')
+
+
 if __name__ == '__main__':
-    # Run the application
-    app.run()
+  app.jinja_env.auto_reload =True
+  app.config['TEMPLATES_AUTO_RELOAD']=True
+  app.run(debug=True)
+ 
